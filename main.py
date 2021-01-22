efe0dceb46343f96f38d0dcced6240609f42579f
Corentin Morvan Chaumeil <40430314+cor3ntino@users.noreply.github.com>
2021-01-22 21:12:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,22 +12,25 @@ Interface(playable=True)
 """
 
 # %% Visualize one of the saved models
-"""
-#neural_net, convolution = Deep_Neural_Network(), False
+
+neural_net, convolution = Deep_Neural_Network(), False # Play with pretrained DNN
+neural_net.load_mod()
+
+#neural_net, convolution = Convolutional_Neural_Network(), True # Play with pretrained CNN
 #neural_net.load_mod()
 
-neural_net, convolution = Convolutional_Neural_Network(), True
-neural_net.load_mod()
-
 agent = Agent_RL(model=neural_net)
-Interface(neural_net=neural_net, agent=agent, playable=False, trainable=False, convolution=convolution)
-"""
-# %% Train Neural Network with Reinforcement Learning
 
+Interface(agent=agent, playable=False, trainable=False, convolution=convolution)
+
+# %% Train Neural Network with Reinforcement Learning
+"""
 #neural_net, convolution = Deep_Neural_Network(), False # Train with DNN
 neural_net, convolution = Convolutional_Neural_Network(), True # Train with CNN
+
 neural_net.load_mod()
 
 agent = Agent_RL(model=neural_net)
-tt(agent)
-#Interface(neural_net=neural_net, agent=agent, playable=False, trainable=True, convolution=convolution)
+
+Interface(agent=agent, playable=False, trainable=True, convolution=convolution)
+"""
